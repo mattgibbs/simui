e30a5e3cad9463b2dcca6c4c70dee17cfa2921c7
Matt Gibbs <mgibbs@slac.stanford.edu>
2019-10-06 15:22:49 -0400

    Magnet displays now load on demand, rather than all at once.

--- area_tabs.py
+++ area_tabs.py
@@ -5,13 +5,14 @@ from utilities import model_list
 from pydm import Display
 from pydm.widgets import PyDMEmbeddedDisplay, PyDMTabWidget
 from qtpy.QtWidgets import QVBoxLayout, QScrollArea, QWidget, QLabel
+from qtpy.QtCore import Slot
 
 class AreaTabs(Display):
     def __init__(self, parent=None, macros=None, args=[]):
         super(AreaTabs, self).__init__(parent=parent, macros=macros, args=args)
         self.selected_area = macros['area']
         self.subsystem = macros['subsystem']
-        self.setWindowTitle("{} {}".format(self.selected_area, self.subsystem))
+        self.setWindowTitle("{} {}".format(self.selected_area.upper(), self.subsystem.upper()))
         self.setup_ui()
         
     
@@ -21,10 +22,24 @@ class AreaTabs(Display):
     def ui_filepath(self):
         return None
     
+    @Slot(int)
+    def tab_changed(self, new_tab_index):
+        emb = self.tab_widget.currentWidget().findChildren(PyDMEmbeddedDisplay)[0]
+        if emb.embedded_widget is not None:
+            print("Area {} already has an embedded widget.".format(self.tab_widget.tabText(new_tab_index)))
+            return
+        top = os.path.dirname(os.path.realpath(__file__))
+        emb.filename = os.path.join(top, self.subsystem, "dev_list_display.py")
+        
+    
     def setup_ui(self):
         self.setLayout(QVBoxLayout())
         self.titleLabel = QLabel(self)
-        self.titleLabel.setText("RF Displays")
+        formatted_subsystem = self.subsystem
+        formatted_subsystem = formatted_subsystem[0].upper() + formatted_subsystem[1:]
+        if formatted_subsystem[-1] == "s":
+            formatted_subsystem = formatted_subsystem[:-1]
+        self.titleLabel.setText("{} Displays".format(formatted_subsystem))
         self.layout().addWidget(self.titleLabel)
         self.tab_widget = PyDMTabWidget(self)
         self.layout().addWidget(self.tab_widget)
@@ -34,11 +49,14 @@ class AreaTabs(Display):
             for i, sector in enumerate(sectors):
                 page = QWidget()
                 page.setLayout(QVBoxLayout())
+                page.layout().setContentsMargins(0,0,0,0)
                 emb = PyDMEmbeddedDisplay()
                 emb.macros = json.dumps(sector)
-                emb.loadWhenShown = True
-                emb.filename = os.path.join(top, self.subsystem, "dev_list_display.py")
+                emb.loadWhenShown = False
+                emb.disconnectWhenHidden = True
                 page.layout().addWidget(emb)
                 self.tab_widget.addTab(page, sector['name'])
                 if sector['name'] == self.selected_area:
                     self.tab_widget.setCurrentIndex(i)
+                    self.tab_changed(i)
+        self.tab_widget.currentChanged.connect(self.tab_changed)
--- magnets/dev_list_display.py
+++ magnets/dev_list_display.py
@@ -4,15 +4,17 @@ from pydm import Display
 from pydm.widgets import PyDMTemplateRepeater, PyDMTabWidget
 from pydm.utilities import establish_widget_connections, close_widget_connections
 from qtpy.QtWidgets import QVBoxLayout, QScrollArea
-from qtpy.QtCore import QSize
+from qtpy.QtCore import QSize, Slot
 from utilities import model_list
 
 def dev_list(start_marker, end_marker, magtype):
+    print("Getting magnet list.")
     l = model_list.model_list(start_element=start_marker, end_element=end_marker)
     mag_dev_list = [i['device_name'] for i in l if i['device_name'].startswith(magtype)]
     #De-duplicate this list using an ordered dict.
     mag_dev_list = list(OrderedDict.fromkeys(mag_dev_list))
     devlist = [{"device_name": devname} for devname in mag_dev_list]
+    print("Mag list is: {}".format(devlist))
     return devlist
 
 class MagnetDeviceList(Display):
@@ -30,52 +32,30 @@ class MagnetDeviceList(Display):
     def ui_filepath(self):
         return None
     
-    def connect(self):
-        if self._is_connected or self.tab_widget is None:
+    @Slot(int)
+    def magtype_tab_changed(self, new_tab_index):
+        print("The magnet tab changed!!")
+        scroll = self.tab_widget.currentWidget()
+        if scroll.widget():
+            print("scroll already has a widget: {}".format(scroll.widget()))
             return
-        print("Connecting!", self.selected_area)
-        establish_widget_connections(self.tab_widget)
-        self._is_connected = True
-
-    def disconnect(self):
-        if not self._is_connected or self.tab_widget is None:
-            return
-        print("Disconnecting!", self.selected_area)
-        close_widget_connections(self.tab_widget)
-        self._is_connected = False
-    
-    def showEvent(self, e):
-        """
-        Show events are sent to widgets that become visible on the screen.
-
-        Parameters
-        ----------
-        event : QShowEvent
-        """
-        self.connect()
-
-    def hideEvent(self, e):
-        """
-        Hide events are sent to widgets that become invisible on the screen.
-
-        Parameters
-        ----------
-        event : QHideEvent
-        """
-        self.disconnect()
+        magtype = self.tab_widget.tabText(new_tab_index)
+        tr = PyDMTemplateRepeater()
+        tr.templateFilename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "magnet-list-item.ui")
+        tr.data = dev_list(self.start_marker, self.end_marker, magtype)
+        scroll.setWidget(tr)
+        tr.show()
     
     def setup_ui(self):
         self.setLayout(QVBoxLayout())
+        self.layout().setContentsMargins(0,0,0,0)
         self.tab_widget = PyDMTabWidget(self)
         self.tab_widget.setTabPosition(PyDMTabWidget.West)
         for magtype in ("QUAD", "XCOR", "YCOR", "BEND"):
             page = QScrollArea()
-            #page.setLayout(QVBoxLayout())
-            tr = PyDMTemplateRepeater()
-            tr.templateFilename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "magnet-list-item.ui")
-            tr.data = dev_list(self.start_marker, self.end_marker, magtype)
-            page.setWidget(tr)
             self.tab_widget.addTab(page, magtype)
         self.layout().addWidget(self.tab_widget)
+        self.magtype_tab_changed(0)
+        self.tab_widget.currentChanged.connect(self.magtype_tab_changed)
         
         
